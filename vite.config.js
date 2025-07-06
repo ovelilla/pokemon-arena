c0460e453a3ff97d5284b4ef9ac16fd3fdfd82e4
Oscar Velilla <o.velillaa@gmail.com>
2025-07-06 13:15:51 +0200

    small fixes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/poke-arena/",
+  base: "/pokemon-arena/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
